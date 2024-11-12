2ba761a1ffc8af701118996d5e88ef0761ea8c23
muhammadahmadazam <139275803+muhammadahmadazam@users.noreply.github.com>
2024-11-12 22:05:04 +0500

    Setup base project

--- src/components/UI/Errors/ErrorBox.js
+++
@@ -1,9 +0,0 @@
-import classes from "./ErrorBox.module.css";
-
-export default function ErrorBox(props) {
-    return (
-        <div className={classes.errorContainer}>
-            <div className={classes.errorMsg}>{props.errorMessage}</div>
-        </div>
-    );
-}
