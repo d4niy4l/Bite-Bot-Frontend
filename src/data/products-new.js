4b02221c4048e5e7f67c4b43746a65d52bf44e73
muhammadahmadazam <139275803+muhammadahmadazam@users.noreply.github.com>
2024-11-21 21:28:55 +0500

    starting frontend for integrating checkout and orders

---
+++ src/data/products-new.js
@@ -0,0 +1,120 @@
+[
+  {
+    name: 'Classic Margherita Pizza',
+    price: 9.99,
+    category: 'Food',
+    availability: true,
+    ingredients: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      'A timeless favorite, our Classic Margherita Pizza features a perfect blend of fresh tomatoes, creamy mozzarella, and fragrant basil on a crispy crust. This pizza is a celebration of simplicity and quality ingredients, making it a go-to choice for pizza lovers everywhere.',
+  },
+  {
+    name: 'Vegan Burger',
+    price: 12.49,
+    category: 'Food',
+    availability: true,
+    ingredients: [{ id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Our Vegan Burger is a delicious and hearty option, made with a plant-based patty, fresh lettuce, juicy tomato, and vegan cheese, all served on a toasted bun. It's a satisfying and flavorful choice for those looking for a meat-free meal without compromising on taste.",
+  },
+  {
+    name: 'Caesar Salad',
+    price: 7.99,
+    category: 'Salad',
+    availability: true,
+    ingredients: [{ id: 8 }, { id: 9 }, { id: 10 }, { id: 11 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Crisp romaine lettuce, crunchy croutons, and shaved parmesan cheese tossed in our creamy Caesar dressing make this Caesar Salad a classic choice. It's a refreshing and satisfying dish that's perfect as a starter or a light meal.",
+  },
+  {
+    name: 'Spaghetti Carbonara',
+    price: 10.99,
+    category: 'Food',
+    availability: false,
+    ingredients: [{ id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Indulge in our rich and creamy Spaghetti Carbonara, made with al dente spaghetti, crispy pancetta, and a luscious egg and parmesan sauce. This classic Italian dish is a comforting and flavorful meal that's sure to satisfy your pasta cravings.",
+  },
+  {
+    name: 'Chicken Tikka Wrap',
+    price: 8.49,
+    category: 'Wraps',
+    availability: true,
+    ingredients: [{ id: 17 }, { id: 18 }, { id: 19 }, { id: 20 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Our Chicken Tikka Wrap is a flavorful combination of tender chicken tikka, fresh cucumber, crisp lettuce, and a tangy yogurt sauce, all wrapped in a soft tortilla. It's a convenient and delicious option for a quick lunch or dinner.",
+  },
+  {
+    name: 'Smoothie Bowl',
+    price: 6.99,
+    category: 'Dessert',
+    availability: true,
+    ingredients: [{ id: 21 }, { id: 22 }, { id: 23 }, { id: 24 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Start your day with our refreshing Smoothie Bowl, packed with bananas, blueberries, crunchy granola, and coconut flakes for a nutritious and delicious treat. It's a perfect blend of flavors and textures that will leave you feeling energized and satisfied.",
+  },
+  {
+    name: 'Avocado Toast',
+    price: 5.49,
+    category: 'Breakfast',
+    availability: false,
+    ingredients: [{ id: 25 }, { id: 26 }, { id: 27 }, { id: 28 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Our Avocado Toast features creamy avocado spread on toasted sourdough bread, topped with cherry tomatoes and a squeeze of fresh lemon juice. It's a simple yet delicious breakfast option that's packed with healthy fats and nutrients.",
+  },
+  {
+    name: 'Pepperoni Pizza',
+    price: 11.99,
+    category: 'Food',
+    availability: true,
+    ingredients: [
+      { id: 29 },
+      { id: 30 },
+      { id: 31 },
+      { id: 32 },
+      { id: 33 },
+      { id: 34 },
+      { id: 35 },
+    ],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Enjoy our Pepperoni Pizza, loaded with savory pepperoni, tomato sauce, mozzarella cheese, chicken, bell peppers, thyme, and a perfectly cooked egg. It's a hearty and satisfying pizza that's perfect for any occasion.",
+  },
+  {
+    name: 'Chocolate Cake',
+    price: 4.99,
+    category: 'Dessert',
+    availability: true,
+    ingredients: [{ id: 36 }, { id: 37 }, { id: 38 }, { id: 39 }, { id: 40 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Indulge in our rich and moist Chocolate Cake, made with high-quality cocoa powder, sugar, butter, and eggs for a decadent dessert experience. It's a perfect treat for chocolate lovers and a great way to end any meal.",
+  },
+  {
+    name: 'Grilled Cheese Sandwich',
+    price: 3.99,
+    category: 'Snacks',
+    availability: true,
+    ingredients: [{ id: 41 }, { id: 42 }, { id: 43 }],
+    imageLink:
+      'https://raw.githubusercontent.com/rohhan36/food-app-assests/main/assets/desert1.png',
+    description:
+      "Our Grilled Cheese Sandwich is a classic comfort food, featuring melted cheddar cheese between two slices of buttery, toasted bread. It's a simple yet satisfying snack that's perfect for any time of day.",
+  },
+];
